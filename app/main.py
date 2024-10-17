5445c72e7d757f28e35db6de8ffde6d0ff82b9ff
danishsheikh1122 <mohammeddanishsheikh1122@gmail.com>
2024-10-18 01:57:58 +0530

    initnt

--- app/main.py
+++ app/main.py
@@ -1,31 +1,26 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Import CORS
 import numpy as np
 import joblib
-
+import flask_cors
 # Load your trained model
 model = joblib.load('rf_model.pkl')
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
-
+flask_cors.CORS(app)
 @app.route('/predict', methods=['POST'])
 def predict():
     # Get the data from the request
     data = request.json
 
-    # Ensure 'prices' key is present in the input data
-    if 'prices' not in data:
-        return jsonify({'error': 'No prices provided'}), 400
-
-    # Convert to NumPy array (assuming your model expects a 1D array)
-    prices = np.array(data['prices']).reshape(1, -1)
+    # Convert to DataFrame (assuming your model expects a DataFrame)
+    data['prices'] = np.array(list(data['prices'].values()))
+    
 
     # Make predictions
-    predictions = model.predict(prices)
-
+    predictions = model.predict(data['prices'].reshape(1,-1))
     # Return the predictions as JSON
     return jsonify(predictions.tolist())
 
 if __name__ == '__main__':
     app.run(debug=True)
+    app.run(debug=False)
